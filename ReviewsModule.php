78c1529f093848ace2cc184d643a5daf8a5cbdb9
Monumentix <brian@monumentixwebdesign.com>
2017-03-11 12:44:09 -0500

    ADDED: composer loading and baseline files

---
+++ ReviewsModule.php
@@ -0,0 +1,10 @@
+<?php
+namespace monumentix\yii2-reviews;
+
+class ReviewsModules extends yii\base\Module{
+
+
+
+}//end class
+
+?>
---
+++ controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace monumentix\reviews\controllers;
+
+use yii\web\Controller;
+
+class IndexController extends Controller {
+
+  public function actionIndex()
+  {
+      return $this->render('index',[
+        'thing'=>'thing',
+      ]);
+  }
+
+
+
+}//end class
